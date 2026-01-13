a3d7eac25af393a8f41bce4626e0fdb5d6e6789c
skulhex <76734977+skulhex@users.noreply.github.com>
2026-01-14 00:19:33 +0500

    update game title to "Shardveil"

--- src/sv/core/config.py
+++ src/sv/core/config.py
@@ -3,4 +3,4 @@ class Settings:
     def __init__(self):
         self.screen_width = 1280
         self.screen_height = 720
-        self.title = "Dungeon Game Prototype"
+        self.title = "Shardveil"
